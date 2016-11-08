f0db0f82ebf6f16cf24414a56e286ad399840f58
Your Name <hyusteoh@umich.edu>
2016-11-07 18:41:59 -0600

    Some modification
    Some progress into bsh3.py

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -1,15 +1,16 @@
-# I MADE A LITTLE CHANGE: HYUNGMOOK STEVE OH HYUSTEOH HOPEFULLY NOTHING BREAKS
-# Use https://www.si.umich.edu/programs/bachelor-science-
-# information/bsi-admissions as a template.
-# STEPS 
-# Create a similar HTML file but 
-# 1) Replace every occurrence of the word “student” with “AMAZING
-# student.”  
-# 2) Replace the main picture with a picture of yourself.
-# 3) Replace any local images with the image I provided in media.  (You
-# must keep the image in a separate folder than your html code.
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
 
-# Deliverables
-# Make sure the new page is uploaded to your GitHub account.
 
-#Just making a change to report on how I'm doing. 11/6/16. Pretty grim, don't know where I should or how I could start the project. However, starting to understand nltk better, just need to go to office hours for more information.
+
+coup = open("steve.html", "w")
+
+base_url = "http://collemc.people.si.umich.edu/data/bshw3StarterFile.html"
+r = urllib.request.urlopen(base_url).read()
+soup = BeautifulSoup(r, 'lxml')
+
+
+soup_string = str(soup)
+print(soup_string)
+coup = coup.write(soup_string)
+
