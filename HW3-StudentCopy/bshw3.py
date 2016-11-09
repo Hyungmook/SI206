fb97aa4bef5ef0a2da9bcd5ab0c222560061e715
Your Name <hyusteoh@umich.edu>
2016-11-08 22:05:15 -0600

    Tried to make some things
    Wished they worked.

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -1,6 +1,7 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
-
+from urllib.request import urlopen
+import requests
 
 
 coup = open("steve.html", "w")
@@ -11,6 +12,18 @@ soup = BeautifulSoup(r, 'lxml')
 
 
 soup_string = str(soup)
-print(soup_string)
+
 coup = coup.write(soup_string)
+
+tags = soup.find_all("div", {"class":"menu-block-wrapper menu-block-1 menu-name-main-menu parent-mlid-0 menu-level-2"})
+
+for t in tags:
+	for i in t.find_all('li'):
+		for x in i.find_all('a'):
+			x_href = x.get("href")
+			if "student" in x_href:
+				print(x_href.replace("students", "AMAZING Student"))
+
+
+				
 
--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -6,3 +6,42 @@
 
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
+
+import tweepy
+import requests
+# Unique code from Twitter
+access_token = "2304113095-9WJv6Q4jwUVurnkd0M92OMR9kRm9POFVI23BsCQ"
+access_token_secret = "en75xKwsthO2d1mgdtpIdHjkBWV1HaZyw6KkPkDT92BLY"
+consumer_key = "sRAjeZ3AQwedfc11WSxH0MS0D"
+consumer_secret = "	nUHPY4b6PedYd6swMyI3wYf4p9TFdvKg8pESJZH5c98PuEV2Iu"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+def tweet_image(url, message):
+    api = tweepy.API(auth)
+    filename = 'temp.jpg'
+    request = requests.get(url, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
+
+        api.update_with_media(filename, status=message)
+        os.remove(filename)
+    else:
+        print("Unable to download image")
+
+
+url = "http://www.rd.com/wp-content/uploads/sites/2/2016/04/05-cat-wants-to-tell-you-bathing-myself.jpg"
+message = "Testing for a class using APIs"
+tweet_image(url, message)
+public_tweets = api.search('UMSI')
+
+
+for tweet in public_tweets:
+	print(tweet.text)
