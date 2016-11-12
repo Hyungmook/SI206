c29bae8c91afe6fbc128ec4e2fe3baae392e57fc
Your Name <hyusteoh@umich.edu>
2016-11-11 23:47:45 -0600

    Last commit
    Last commit

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -11,6 +11,10 @@ import tweepy
 import requests
 # Unique code from Twitter
 
+access_token = "2304113095-9WJv6Q4jwUVurnkd0M92OMR9kRm9POFVI23BsCQ"
+access_token_secret = "en75xKwsthO2d1mgdtpIdHjkBWV1HaZyw6KkPkDT92BLY"
+consumer_key = "sRAjeZ3AQwedfc11WSxH0MS0D"
+consumer_secret = "nUHPY4b6PedYd6swMyI3wYf4p9TFdvKg8pESJZH5c98PuEV2Iu"
 
 
 # Boilerplate code here
